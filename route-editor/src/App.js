e69d1db02d53ec0915ac3414fa2d6387c4745de5
gulyashik <guliaeva.olya2014@yandex.ru>
2019-08-23 23:35:03 +0300

    add Ymap

--- route-editor/src/App.js
+++ route-editor/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { YMaps, Map } from 'react-yandex-maps';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <YMaps>
+    <div>
+      My awesome application with maps!
+      <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
     </div>
+  </YMaps>
   );
 }
 
